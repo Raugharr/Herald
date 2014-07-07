803a80f602e184ffe2849e696cbb7997de6e5c82
unknown <drbrotz@yahoo.com>
2014-07-07 01:37:53 -0500

    Added basic animals.

--- data/populations.lua
+++ data/populations.lua
@@ -1,3 +1,25 @@
 Populations = {
-
+	{
+		Name = "Sheep",
+		AdultFood = 9,
+		ChildFood = 4,
+		BreedAge = {1, 7},
+		DeathAge = 12,
+		MaleRatio = 0.2,
+		Eats = {"Hay", "Straw"},
+		Milk = 68, --Fluid ounces.
+		Meat = 45
+	},
+	
+	{
+		Name = "Ox",
+		AdultFood = 45,
+		ChildFood = 22,
+		BreedAge = {1, 15},
+		DeathAge = 20,
+		MaleRatio = 0.2,
+		Eats = {"Hay", "Straw"},
+		Milk = 120,
+		Meat = 1600
+	}
 }

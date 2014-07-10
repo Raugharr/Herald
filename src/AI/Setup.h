e45ac6c2e659935d4a417963fd6e6e008547c82c
unknown <drbrotz@yahoo.com>
2014-07-09 19:13:33 -0500

    Added AI functions to Setup.h

--- src/AI/Setup.h
+++ src/AI/Setup.h
@@ -7,6 +7,9 @@
 #define __SETUP_H
 
 struct Behavior;
+struct Person;
+struct HashTable;
+struct Array;
 
 #define AI_MAKEGOOD "MakeGood"
 #define AI_MAKEBUILD "MakeBuild"
@@ -18,6 +21,22 @@ struct Behavior;
 extern struct Behavior* g_AIMan;
 extern struct Behavior* g_AIWoman;
 extern struct Behavior* g_AIChild;
+
+int PAIHasField(struct Person* _Person, struct HashTable* _Table);
+int PAIHasHouse(struct Person* _Person, struct HashTable* _Table);
+int PAIWorkField(struct Person* _Person, struct HashTable* _Table);
+int PAIBuildHouse(struct Person* _Person, struct HashTable* _Table);
+int PAICanFarm(struct Person* _Person, struct HashTable* _Table);
+int PAIHasPlow(struct Person* _Person, struct HashTable* _Table);
+int PAIMakeGood(struct Person* _Person, struct HashTable* _Table);
+int PAIHasReap(struct Person* _Person, struct HashTable* _Table);
+int PAIHasAnimals(struct Person* _Person, struct HashTable* _Table);
+int PAIConstructBuild(struct Person* _Person, struct HashTable* _Table);
+int PAIHasShelter(struct Person* _Person, struct HashTable* _Table);
+int PAIFeedAnimals(struct Person* _Person, struct HashTable* _Table);
+int PAIEat(struct Person* _Person, struct HashTable* _Table);
+int PAIMakeFood(struct Person* _Person, struct HashTable* _Table);
+int BHVNothing(struct Person* _Person, struct HashTable* _Table);
 
 void AIInit();
 void AIQuit();

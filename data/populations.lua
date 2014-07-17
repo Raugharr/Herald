e7d8e64443d58c6c8f3b72673b0ebd864bd042fe
unknown <drbrotz@yahoo.com>
2014-07-16 23:53:27 -0500

    Updated Populations data.

--- data/populations.lua
+++ data/populations.lua
@@ -1,9 +1,8 @@
 Populations = {
 	{
 		Name = "Sheep",
-		AdultFood = 9,
-		ChildFood = 4,
-		BreedAge = {1, 7},
+		Nutrition = 18,
+		MatureAge = {1, 7},
 		DeathAge = 12,
 		MaleRatio = 0.2,
 		Eats = {"Hay", "Straw"},
@@ -13,13 +12,45 @@ Populations = {
 	
 	{
 		Name = "Ox",
-		AdultFood = 45,
-		ChildFood = 22,
-		BreedAge = {1, 15},
+		Nutrition = 90,
+		MatureAge = {1, 15},
 		DeathAge = 20,
 		MaleRatio = 0.2,
 		Eats = {"Hay", "Straw"},
 		Milk = 120,
 		Meat = 1600
+	},
+	
+	{
+		Name = "Pig",
+		Nutrition = 18,
+		MatureAge = {1, 4},
+		DeathAge = 16,
+		MaleRatio = 0.25,
+		Eats = {"Barley", "Oats"},
+		Milk = 10,
+		Meat = 45
+	},
+	
+	{
+		Name = "Chicken",
+		Nutrition = 2,
+		MatureAge = {1, 3},
+		DeathAge = 5,
+		MaleRatio = 0.05,
+		Eats = {"Barley", "Oats"},
+		Milk = 0,
+		Meat = 3
+	},
+	
+	{
+		Name = "Goat",
+		Nutrition = 10,
+		MatureAge = {1, 6},
+		DeathAge = 10,
+		MaleRatio = 0.2,
+		Eats = {"Barley", "Oats", "Hay", "Straw"},
+		Milk = 12,
+		Meat = 18,
 	}
 }

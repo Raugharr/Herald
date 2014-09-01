0b121a8bba557c5c7befe3ce8618fc4f77bb3bf9
unknown <drbrotz@yahoo.com>
2014-09-01 12:32:23 -0500

    Goods.lua takes up fewer lines.

--- data/goods.lua
+++ data/goods.lua
@@ -1,97 +1,18 @@
 Goods = {
-	{
-		Name = "Iron",
-		Category = "Material",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Log",
-		Category = "Material",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Board",
-		Category = "Material",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Stone",
-		Category = "Material",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Wool",
-		Category = "Other",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Flour",
-		Category = "Ingredient",
-		InputGoods = {{"Wheat", 1}}
-	},
-	
-	{
-		Name = "Bread Loaf",
-		Category = "Food",
-		InputGoods = {{"Flour", 12}}
-	},
-	
-	{
-		Name = "Straw",
-		Category = "Food",
-		InputGoods = {},
-		Nutrition = 3 --Per pound
-	},
-	
-	{
-		Name = "Hay",
-		Category = "Food",
-		InputGoods = {},
-		Nutrition = 5 --Per pound
-	},
-	
-	{
-		Name = "Meat",
-		Category = "Food",
-		InputGoods = {},
-		Nutrition = 3 --Per pound, exception is hard coded.
-	},
-	
-	{
-		Name = "Stone Sickle",
-		Category = "Tool",
-		Function = "Reap",
-		InputGoods = {}
-	},
-	
-	{
-		Name = "Shear",
-		Category = "Tool",
-		Function = "Cut",
-		InputGoods = {{"Iron", 2}, {"Wood", 2}} 
-	},
-	
-	{
-		Name = "Scratch Plow",
-		Category = "Tool",
-		Function = "Plow",
-		InputGoods = {{"Wood", 6}} 
-	},
-	
-	{
-		Name = "Wool Trousers",
-		Category = "Clothing",
-		InputGoods = {{"Wool", 12}}
-	},
-	
-	{
-		Name = "Wool Tunic",
-		Category = "Clothing",
-		InputGoods = {{"Wool", 12}}
-	}
+	{Name = "Axe", Category = "Tool", Function = "Logging", InputGoods = {}},
+	{Name = "Board", Category = "Material", InputGoods = {}},
+	{Name = "Bread Loaf", Category = "Food", InputGoods = {{"Flour", 12}}},
+	{Name = "Flour", Category = "Ingredient", InputGoods = {{"Wheat", 1}}},
+	{Name = "Iron", Category = "Material", InputGoods = {}},
+	{Name = "Hay", Category = "Food", InputGoods = {}, Nutrition = 5},
+	{Name = "Log", Category = "Material", InputGoods = {}},
+	{Name = "Meat", Category = "Food", InputGoods = {}, Nutrition = 3},
+	{Name = "Shear", Category = "Tool", Function = "Cut", InputGoods = {{"Iron", 2}, {"Wood", 2}}},
+	{Name = "Scratch Plow", Category = "Tool", Function = "Plow", InputGoods = {{"Wood", 6}}},
+	{Name = "Stone", Category = "Material", InputGoods = {}},
+	{Name = "Stone Sickle", Category = "Tool", Function = "Reap", InputGoods = {}},
+	{Name = "Straw", Category = "Food", InputGoods = {}, Nutrition = 3},
+	{Name = "Wool", Category = "Other", InputGoods = {}},
+	{Name = "Wool Tunic", Category = "Clothing", InputGoods = {{"Wool", 12}}},
+	{Name = "Wool Trousers", Category = "Clothing", InputGoods = {{"Wool", 12}}}
 }

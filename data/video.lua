5a7bd04dbe9a412c101a37ad09f0418605cd35a9
unknown <drbrotz@yahoo.com>
2014-10-14 23:30:50 -0500

    Added SetFocusColor and SetUnfocusColor.

--- data/video.lua
+++ data/video.lua
@@ -1,5 +1,6 @@
 function GUI.Init()
 	GUI.SetFont(GUI.GetFont("Elementary_Gothic_Bookhand.ttf", 12))
-	GUI.SetColor(255, 255, 255)
+	GUI.SetFocusColor(255, 255, 255)
+	GUI.SetUnfocusColor(128, 128, 128)
 	GUI.SetMenu("MainMenu")
 end

5edb64b2c3eba49fce930a56b2cf55c6fd473f1a
unknown <drbrotz@yahoo.com>
2014-10-14 23:31:16 -0500

    Changed background color.

--- data/gui/MainMenu.lua
+++ data/gui/MainMenu.lua
@@ -3,7 +3,7 @@ MainMenu = { }
 function MainMenu.Init(Width, Height)
 	MainMenu.Screen = GUI.VerticalContainer(0, 0, Width, Height, 2, {50, 50, 0, 0})
 	
-	GUI.BackgroundColor(255, 0, 0)
+	GUI.BackgroundColor(0, 0, 0)
 	MainMenu.Screen:CreateTextBox("New")
 	MainMenu.Screen:CreateTextBox("Load")
 	MainMenu.Screen:CreateTextBox("Debug"):OnKey("Enter", "Released", 

6df716121644b1e33584e429ee6d36783b29be8c
unknown <drbrotz@yahoo.com>
2014-11-04 08:52:18 -0600

    Added menu to view the data of a person.

---
+++ data/gui/ViewPersonMenu.lua
@@ -0,0 +1,51 @@
+ViewPersonMenu = { }
+
+function ViewPersonMenu.Init(Width, Height, Person)
+	local Screen = GUI.HorizontalContainer(0, 0, Width, Height, 0, {0, 0, 0, 0})
+	local Menu = GUI.VerticalContainer(0, 0, 512, Height, 0, {0, 0, 0, 0}, Screen)
+	Display = nil
+	local String = Person.Name .. " is of the family " .. Person.Family:GetName() .. "."
+	
+	Menu:CreateTextBox(String):SetFocus(false)
+	Menu:CreateTextBox("Skills")
+	Menu:CreateTextBox("Agriculture"):OnKey("Enter", "Released",
+		function()
+			local Field = Person.Family:GetField()
+			
+			if(Display ~= nil) then
+				Display:Destroy()
+			end
+			Display = Screen:CreateTable(3, 16, 0, {0, 0, 0, 0})
+			Display:SetCellWidth(Display:GetFont():FontWidth() * 8)
+			Display:SetCellHeight(Display:GetFont():FontHeight())
+			
+			Display:CreateTextBox("Name"):SetFocus(false)
+			Display:CreateTextBox("Yield"):SetFocus(false)
+			Display:CreateTextBox("Acres"):SetFocus(false)
+			Display:CreateTextBox(Field:GetCrop().Name)
+			Display:CreateTextBox(Field:GetYield())
+			Display:CreateTextBox(Field:GetAcres())
+		end)
+	Menu:CreateTextBox("Goods"):OnKey("Enter", "Released",
+		function()
+			if(Display ~= nil) then
+				Display:Destroy()
+			end
+			Display = Screen:CreateTable(2, 16, 0, {0, 0, 0, 0})
+			Display:SetCellWidth(Display:GetFont():FontWidth() * 8)
+			Display:SetCellHeight(Display:GetFont():FontHeight())
+			Display:CreateTextBox("Name"):SetFocus(false)
+			Display:CreateTextBox("Quantity"):SetFocus(false)
+			for Good in Person.Family:GetGoods():Next() do
+				Display:CreateTextBox(Good:GetBase().Name)
+				Display:CreateTextBox(Good:GetQuantity())
+			end
+		end)
+	Menu:CreateTextBox("Animals")
+	Menu:CreateTextBox("Back"):OnKey("Enter", "Released",
+		function()
+			GUI.Close()
+		end)
+	
+	return false
+end

25faced425c5dbbf8ba5d5ad2085ed06722de934
unknown <drbrotz@yahoo.com>
2015-02-05 20:29:05 -0600

    Added simple title to main menu.

--- data/gui/MainMenu.lua
+++ data/gui/MainMenu.lua
@@ -1,10 +1,14 @@
 MainMenu = { }
 
 function MainMenu.Init(Width, Height, Data)
-	MainMenu.Screen = GUI.VerticalContainer(0, 0, Width, Height, 2, {50, 50, 0, 0})
+	MainMenu.Screen = GUI.VerticalContainer(0, 0, Width, Height, 0, {50, 50, 0, 0})
 	
 	GUI.BackgroundColor(0, 0, 0)
-	Title = MainMenu.Screen:CreateTextBox("Herald"):SetFocus(false)
+	local TitleCon = GUI.HorizontalContainer(0, 0, Width, 30, 0, {0, 0, 0, 0}, MainMenu.Screen)
+	TitleCon:SetFocus(false)
+	local Title = TitleCon:CreateTextBox("Herald")
+	Title:SetFocus(false)
+	Title:SetX(TitleCon:GetHorizontalCenter(Title))
 	MainMenu.Screen:CreateTextBox("New"):OnKey("Enter", "Released",
 		function()
 			GUI.SetMenu("GameMenu")

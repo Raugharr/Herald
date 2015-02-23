e5b582d100128cd318840aa672b2e07e0d32839d
unknown <drbrotz@yahoo.com>
2015-02-22 19:02:45 -0600

    Added pool of threads to handle tasks.

---
+++ src/sys/TaskPool.c
@@ -0,0 +1,87 @@
+/*
+ * File: TaskPool.c
+ * Author: David Brotz
+ */
+
+#include "TaskPool.h"
+
+#include "MemoryPool.h"
+
+#include <stdlib.h>
+#include <SDL2/SDL_thread.h>
+#include <SDL2/SDL.h>
+
+int TaskCmp(const void* _One, const void* _Two) {
+	return ((struct Task*)_Two)->StartTime - ((struct Task*)_One)->StartTime;
+}
+
+int TaskPoolThread(struct TaskPool* _TaskPool) {
+	struct Task* _Task = NULL;
+
+	//SDL_LockMutex(_TaskPool->ThreadMutex);
+	while(_TaskPool->IsAlive) {
+		while((_Task = TaskPoolNext(_TaskPool)) == NULL)
+			SDL_Delay(10);
+		//	SDL_CondWaitTimeout(_TaskPool->ThreadCon, _TaskPool->ThreadMutex, 10);
+		_Task->Callback(_Task->DataOne, _Task->DataTwo);
+	}
+	//SDL_UnlockMutex(_TaskPool->ThreadMutex);
+	return 1;
+}
+
+struct TaskPool* CreateTaskPool() {
+	struct TaskPool* _TaskPool = (struct TaskPool*) malloc(sizeof(struct TaskPool));
+	int i = 0;
+
+	_TaskPool->ThreadCt = 2;
+	_TaskPool->Schedule.Table = calloc(1024, sizeof(struct Task));
+	_TaskPool->Schedule.TblSz = 1024;
+	_TaskPool->Schedule.Size = 0;
+	_TaskPool->Schedule.Compare = &TaskCmp;
+	_TaskPool->TaskMemPool = CreateMemoryPool(sizeof(struct Task), 1000);
+	_TaskPool->Threads = calloc(_TaskPool->ThreadCt, sizeof(SDL_Thread*));
+	_TaskPool->IsAlive = 1;
+	_TaskPool->Time = 0;
+	_TaskPool->PoolMutex = SDL_CreateMutex();
+	for(i = 0; i < _TaskPool->ThreadCt; ++i)
+		_TaskPool->Threads[i] = SDL_CreateThread(((int(*)(void*))&TaskPoolThread), "TaskPoolThread", _TaskPool);
+	return _TaskPool;
+}
+
+void DestroyTaskPool(struct TaskPool* _TaskPool) {
+	int i = 0;
+
+	_TaskPool->IsAlive = 0;
+	for(i = 0; i < _TaskPool->ThreadCt; ++i)
+		SDL_WaitThread(_TaskPool->Threads[i], NULL);
+	free(_TaskPool->Threads);
+	for(i = 0; i < _TaskPool->Schedule.Size; ++i)
+		MemPoolFree(_TaskPool->TaskMemPool, _TaskPool->Schedule.Table[i]);
+	free(_TaskPool->Schedule.Table);
+	DestroyMemoryPool(_TaskPool->TaskMemPool);
+	SDL_DestroyMutex(_TaskPool->PoolMutex);
+	free(_TaskPool);
+}
+
+struct Task* TaskPoolNext(struct TaskPool* _Pool) {
+	struct Task* _Task = NULL;
+
+	SDL_LockMutex(_Pool->PoolMutex);
+	if(_Pool->Schedule.Size > 0 && ((struct Task*)_Pool->Schedule.Table[0])->StartTime <= _Pool->Time)
+		_Task = (struct Task*)BinaryHeapPop(&_Pool->Schedule);
+	SDL_UnlockMutex(_Pool->PoolMutex);
+	return _Task;
+}
+
+void TaskPoolAdd(struct TaskPool* _Pool, int _StartTime, void (*_Callback)(void*, void*), void* _DataOne, void* _DataTwo) {
+	struct Task* _Task = NULL;
+
+	SDL_LockMutex(_Pool->PoolMutex);
+	_Task = MemPoolAlloc(_Pool->TaskMemPool);
+	_Task->StartTime = _StartTime;
+	_Task->Callback = _Callback;
+	_Task->DataOne = _DataOne;
+	_Task->DataTwo = _DataTwo;
+	BinaryHeapInsert(&_Pool->Schedule, _Task);
+	SDL_UnlockMutex(_Pool->PoolMutex);
+}
---
+++ src/sys/TaskPool.h
@@ -0,0 +1,42 @@
+/*
+ * File: TaskPool.h
+ * Author: David Brotz
+ */
+#ifndef __TASKPOOL_H
+#define __TASKPOOL_H
+
+#include "BinaryHeap.h"
+
+typedef struct SDL_Thread SDL_Thread;
+typedef struct SDL_con SDL_con;
+typedef struct SDL_mutex SDL_mutex;
+
+struct Task {
+	void (*Callback)(void*, void*);
+	void* DataOne;
+	void* DataTwo;
+	int StartTime;
+};
+
+struct TaskPool {
+	struct BinaryHeap Schedule;
+	struct MemoryPool* TaskMemPool;
+	unsigned int Clock;
+	int ThreadCt;
+	int IsAlive;
+	int Time;
+	SDL_Thread** Threads;
+	//SDL_con* ThreadCon;
+	//SDL_mutex* ThreadMutex;
+	SDL_mutex* PoolMutex;
+};
+
+int TaskCmp(const void* _One, const void* _Two);
+
+struct TaskPool* CreateTaskPool();
+void DestroyTaskPool(struct TaskPool* _TaskPool);
+
+struct Task* TaskPoolNext(struct TaskPool* _Pool);
+void TaskPoolAdd(struct TaskPool* _Pool, int _StartTime, void (*_Callback)(void*, void*), void* _DataOne, void* _DataTwo);
+
+#endif

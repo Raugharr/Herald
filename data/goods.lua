51b6bb3c0bd06e2257a1cf92b91340d2f12978f7
David Brotz <Drbrotz@yahoo.com>
2015-02-25 20:34:36 -0600

    Fixed compile errors.

--- data/goods.lua
+++ data/goods.lua
@@ -13,10 +13,10 @@ Goods = {
 	{Name = "Stone Sickle", Category = "Tool", Function = "Reap", InputGoods = {}},
 	{Name = "Straw", Category = "Food", InputGoods = {}, Nutrition = 3},
 	{Name = "Wool", Category = "Other", InputGoods = {}},
-	{Name = "Wool Yarn", Category = "Material", InputGoods = {"Wool", 1}},
+	{Name = "Wool Yarn", Category = "Material", InputGoods = {{"Wool", 1}}},
 	{Name = "Wool Tunic", Category = "Clothing", Locations = {"Upper Arms", "Upper Chest", "Lower Chest"}, InputGoods = {{"Wool Yarn", 57600}}},
 	{Name = "Wool Trousers", Category = "Clothing", Locations = {"Ankles", "Lower Legs", "Upper Legs", "Pelvis"}, InputGoods = {{"Wool Yarn", 57600}}},
-	{Name = "Quern", Category = "Good", InputGoods = {{"Stone", 3}} OutputGoods = {{"Flour", 300}}
+	{Name = "Quern", Category = "Good", InputGoods = {{"Stone", 3}}, OutputGoods = {{"Flour", 300}}},
 	{Name = "Dirt", Category = "Material", InputGoods = {}},
 	{Name = "Wood", Category = "Material", InputGoods = {}}
 }

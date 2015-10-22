b5c3b23f3bfcc36d6d7a4e722eb93a9268c67c24
unknown <drbrotz@yahoo.com>
2015-10-22 15:41:49 -0500

    Added GenIterator functionality to handle arrays, linked lists, and RBTrees.

---
+++ src/sys/GenIterator.c
@@ -0,0 +1,65 @@
+/*
+ * File: GenIterator.c
+ * Author: David Brotz
+ */
+
+#include "GenIterator.h"
+
+#include "RBTree.h"
+#include "LinkedList.h"
+
+#include <stdlib.h>
+
+struct GenIterator* CreateRBItr(struct RBTree* _Tree, int _Size) {
+	struct GenIterator* _Iterator = (struct GenIterator*) malloc(sizeof(struct GenIterator));
+
+	_Iterator->HasNext = GenIteratorArrayHasNext;
+	_Iterator->NextObj = GenIteratorArrayNextObj;
+	_Iterator->StackSz = _Size;
+	_Iterator->Stack = calloc(_Size, sizeof(void*));
+	_Iterator->Index = 0;
+	return _Iterator;
+}
+
+void DestroyRBItr(struct GenIterator* _Iterator) {
+	free(_Iterator->Stack);
+	free(_Iterator);
+}
+
+struct GenIterator* CreateArrayItr(void* _Array, int _Size) {
+	struct GenIterator* _Iterator = (struct GenIterator*) malloc(sizeof(struct GenIterator));
+
+	_Iterator->HasNext = GenIteratorArrayHasNext;
+	_Iterator->NextObj = GenIteratorArrayNextObj;
+	_Iterator->StackSz = _Size;
+	_Iterator->Stack = _Array;
+	_Iterator->Index = 0;
+	return _Iterator;
+}
+
+void DestroyArrayItr(struct GenIterator* _Iterator) {
+	free(_Iterator);
+}
+
+int GenIteratorArrayHasNext(struct GenIterator* _Iterator) {
+	return (_Iterator->Index < _Iterator->StackSz);
+}
+
+void* GenIteratorArrayNextObj(struct GenIterator* _Iterator) {
+	void* _Obj = ((void**)_Iterator->Stack)[_Iterator->Index];
+
+	++_Iterator->Index;
+	return _Obj;
+}
+
+int GenIteratorListHasNext(struct GenIterator* _Iterator) {
+	return (_Iterator->Stack == NULL);
+}
+
+void* GenIteratorListNextObj(struct GenIterator* _Iterator) {
+	void* _Obj = ((struct LnkLst_Node*)_Iterator->Stack)->Data;
+
+	_Iterator->Stack = ((struct LnkLst_Node*)_Iterator->Stack)->Next;
+	++_Iterator->Index;
+	return _Obj;
+}
---
+++ src/sys/GenIterator.h
@@ -0,0 +1,32 @@
+/*
+ * File: GenIterator.h
+ * Author: David Brotz
+ */
+#ifndef __GENITERATOR_H
+#define __GENITERATOR_H
+
+/*
+ * Generic container for any object that can be iterated over.
+ */
+struct RBTree;
+
+struct GenIterator {
+	int (*HasNext)(struct GenIterator*);
+	void* (*NextObj)(struct GenIterator*);
+	void* Stack;
+	int Index;
+	int StackSz;
+};
+struct GenIterator* CreateRBItr(struct RBTree* _Tree, int _Size);
+void DestroyRBItr(struct GenIterator* _Iterator);
+
+struct GenIterator* CreateArrayItr(void* _Array, int _Size);
+void DestroyArrayItr(struct GenIterator* _Iterator);
+
+int GenIteratorArrayHasNext(struct GenIterator* _Iterator);
+void* GenIteratorArrayNextObj(struct GenIterator* _Iterator);
+
+int GenIteratorListHasNext(struct GenIterator* _Iterator);
+void* GenIteratorListNextObj(struct GenIterator* _Iterator);
+
+#endif /* GENITERATOR_H_ */

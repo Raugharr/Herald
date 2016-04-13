4f6f3a7ad9c4dd7d03e5b8487360cc6463f47a24
David Brotz <Drbrotz@yahoo.com>
2016-04-13 18:27:41 -0500

    Removed Menu.Screen made MissionMenu conform to the new way of using menu's.

--- data/gui/MissionMenu.lua
+++ data/gui/MissionMenu.lua
@@ -2,25 +2,29 @@ Menu.__savestate = false;
 Menu.moveable = false;
 
 function Menu.Init(Menu, Data)
-	Menu.Screen = GUI.VerticalContainer(0, 0, Menu:GetWidth(), Menu:GetHeight())
 	Menu.Mission = Data["Mission"]
 	Menu.Data = Data["Data"]
 	
-	Menu.TitleCon = GUI.HorizontalContainer(0, 0, Menu:GetWidth(), 30, Menu.Screen)
+	Menu.TitleCon = GUI.HorizontalContainer(0, 0, Menu:GetWidth(), 30, Menu)
 	Menu.TitleCon:SetFocus(false)
 	Menu.Title = Menu.TitleCon:CreateLabel(Menu.Mission:GetName())
 	Menu.Title:SetFocus(false)
 	Menu.Title:SetX(Menu.TitleCon:GetHorizontalCenter(Menu.Title))
-	Menu.Screen:Paragraph(Data["Description"])
+	Menu.Description = Menu:Paragraph(Data["Description"])
+	Menu.Description:Below(Menu.TitleCon)
+	Menu.ButtonCont = GUI.VerticalContainer(0, 0, Menu:GetWidth(), Menu:GetHeight(), Menu)
+	Menu.ButtonCont:Below(Menu.Description)
 	for k, v in ipairs(Menu.Mission:GetOptions()) do
 		if v:ConditionSatisfied() == true then
-			Menu.Screen:CreateButton(v:GetName(),
+			Menu.ButtonCont:CreateButton(v:GetName(),
 			function()
 				Menu.Mission:ChooseOption(Menu.Data, k - 1)
-				Menu.Screen:Close()
+				Menu:Close()
 			end)
 		end
 	end
+	Menu.ButtonCont:Shrink()
+	--Menu.Description:Below(Menu.ButtonCont)
 end
 
 function Menu.Think(Menu)

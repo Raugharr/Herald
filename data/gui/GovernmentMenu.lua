402f2552b0d86bff145c43d4c805d35fa4a944fd
David Brotz <Drbrotz@yahoo.com>
2016-05-07 20:54:25 -0500

    Changed GovernmenyMenu to no longer use a screen but to use Menu as its main container.

--- data/gui/GovernmentMenu.lua
+++ data/gui/GovernmentMenu.lua
@@ -2,35 +2,31 @@ Menu.__savestate = false;
 Menu.moveable = true;
 
 function Menu.Init(Menu, Data)
-	Menu.Screen = GUI.VerticalContainer(0, 0, 512, Menu:GetHeight(), 0, {0, 0, 0, 0})
-	Menu.VoteCont = GUI.VerticalContainer(0, 0, 512, 100, 0, {0, 0, 0, 0}, Menu.Screen)
+--	Menu.VoteCont = GUI.VerticalContainer(0, 0, 512, 100, 0, {0, 0, 0, 0})
 	Menu.Government = Data["Settlement"]
-	Menu.Reform = Menu.Government:GetReform()
+--	Menu.Reform = Menu.Government:GetReform()
 	
-	Menu.TitleCon = GUI.HorizontalContainer(0, 0, Menu:GetWidth(), 30, 0, {0, 0, 0, 0}, Menu.Screen)
+	Menu.TitleCon = GUI.HorizontalContainer(0, 0, Menu:GetWidth(), 30, 0, {0, 0, 0, 0}, Menu)
 	Menu.TitleCon:SetFocus(false)
 	Menu.Title = Menu.TitleCon:CreateLabel("Government")
 	Menu.Title:SetFocus(false)
 	Menu.Title:SetX(Menu.TitleCon:GetHorizontalCenter(Menu.Title))
-	--Screen:Paragraph(Mission.Description)
-	--Government:PossibleReforms():Front():Data():GetName()
-	Menu.VoteCont:SetFocus(false)
-	if Menu.Reform ~= nil then
-		Menu.VoteLabel = Menu.VoteCont:CreateLabel(Menu.Reform:GetVotes() .. " votes for passing  out of " .. Menu.Reform:GetMaxVotes() .. " total votes.")
-	end
+--	Menu.VoteCont:SetFocus(false)
+--	if Menu.Reform ~= nil then
+--		Menu.VoteLabel = Menu.VoteCont:CreateLabel(Menu.Reform:GetVotes() .. " votes for passing  out of " .. Menu.Reform:GetMaxVotes() .. " total votes.")
+--	end
 	
-	Menu.Screen:CreateLabel(Menu.Government:Rule() .. " " .. Menu.Government:Structure() .. " " .. Menu.Government:Type()):SetFocus(false)
+	Menu:CreateLabel(Menu.Government:Rule() .. " " .. Menu.Government:Structure() .. " " .. Menu.Government:Type()):SetFocus(false)
 	for v in Menu.Government:PossibleReforms():Front() do
-		Menu.Screen:CreateButton(v:GetName(),
+		Menu:CreateButton(v:GetName(),
 			function()
 				Menu.Government:PassReform(v)
 			 end)
 	end
-	Menu.Screen:CreateButton("Back",
+	Menu:CreateButton("Back",
 		function()
 			GUI.PopMenu()
 		end)
-	return Menu.Screen
 end
 
 function Menu.Think(Menu)

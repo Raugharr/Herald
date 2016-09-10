57a83dc8e1e4530fed0e2f4e30eb9f4961b01be2
David Brotz <Drbrotz@yahoo.com>
2016-09-09 22:18:23 -0500

    Change: A family can now only have one profession.

---
+++ src/Profession.h
@@ -0,0 +1,35 @@
+/*
+ * File: Profession.h 
+ * Author: David Brotz
+ */
+#ifndef __PAYMENT_H
+#define __PAYMENT_H
+
+#include <inttypes.h>
+
+struct GoodBase;
+typedef struct lua_State lua_State;
+
+enum PAYMENT {
+	PAYMENT_MONEY = (1 << 0),
+	PAYMENT_INKIND = (1 << 1),
+	PAYMENT_SERVICE = (1 << 2)
+};
+
+struct Profession {
+	uint32_t Type;
+	const char* Name;
+	const struct GoodBase** CraftedGoods;
+	const struct GoodBase** InputGoods;
+	uint32_t Payment;
+	uint8_t CraftedGoodCt;
+	uint8_t InputGoodCt;
+};
+
+struct Profession* CreateProfession(const char* _Name, const struct GoodBase** _CraftedGoods, int _Payment);
+void DestroyProfession(struct Profession* _Profession);
+
+struct Profession* LoadProfession(lua_State* _State, int _Index);
+const struct GoodBase** LoadInKind(lua_State* _State, int _Index);
+int ProfessionPaymentType(const char* _Str);
+#endif
